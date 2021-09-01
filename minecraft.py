f468d7b7dc60563d1412fae36e981930d2a7aca4
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-09-01 13:33:25 +0200

    Updated minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -1,46 +1,5 @@
 import keyboard
 
-class player :
-    
-    def playerDatas() :
-        
-        playerXCord = 2
-        playerYCord = 0
-        playerZCord = 0
-        playerCameraXCord = 2
-        playerCameraYCord = 0
-        playerCameraZCord = 0
-        def playerSettings() :
-            
-            playerForwardButton = "z" AND "Z"
-            playerLeftButton = "q" AND "Q"
-            playerRightButton = "d" AND "D"
-            playerBackButton = "s" AND "S"
-            playerShiftButton = ""
-            
-    def move(key) :
-        
-        if key == playerDatas.playerSettings.playerForwardButton :
-            
-            
-            
-        if key == playerDatas.playerSettings.playerLeftButton :
-            
-            
-            
-        if key == playerDatas.playerSettings.playerRightButton :
-            
-            
-            
-        if key == playerDatas.playerSettings.playerBackButton :
-            
-            
-            
-        if key == playerDatas.playerSettings.playerShiftButton :
-            
-            game.animations.shift
-            playerCameraXCord = playerXCord -0.5
-            
 class game :
     
     def animations() :
@@ -83,3 +42,53 @@ class game :
         cobbleBlock = {"name": "cobble", "id": "", "images": {"topImage": {"img": "", "size": {"x": "64", "y": "64"}}, "bottomImage": {"img": "", "size": {"x": "64", "y": "64"}}, "forwardImage": {"img": "", "size": {"x": "64", "y": "64"}}, "backImage": {"img": "", "size": {"x": "64", "y": "64"}}, "leftImage": {"img": "", "size": {"x": "64", "y": "64"}}, "rightImage": {"img": "", "size": {"x": "64", "y": "64"}}}}
         cobblestoneBlock = {"name": "cobble stone", "id": "", "images": {"topImage": {"img": "", "size": {"x": "64", "y": "64"}}, "bottomImage": {"img": "", "size": {"x": "64", "y": "64"}}, "forwardImage": {"img": "", "size": {"x": "64", "y": "64"}}, "backImage": {"img": "", "size": {"x": "64", "y": "64"}}, "leftImage": {"img": "", "size": {"x": "64", "y": "64"}}, "rightImage": {"img": "", "size": {"x": "64", "y": "64"}}}}
         lightBlock = {"name": "light", "id": "", "images": {"topImage": {"img": "", "size": {"x": "64", "y": "64"}}, "bottomImage": {"img": "", "size": {"x": "64", "y": "64"}}, "forwardImage": {"img": "", "size": {"x": "64", "y": "64"}}, "backImage": {"img": "", "size": {"x": "64", "y": "64"}}, "leftImage": {"img": "", "size": {"x": "64", "y": "64"}}, "rightImage": {"img": "", "size": {"x": "64", "y": "64"}}}}
+        
+    def gui() :
+        
+        
+class player :
+    
+    def playerDatas() :
+        
+        playerXCord = 2
+        playerYCord = 0
+        playerZCord = 0
+        playerCameraXCord = 2
+        playerCameraYCord = 0
+        playerCameraZCord = 0
+        def playerSettings() :
+            
+            playerForwardButton = "z" AND "Z"
+            playerLeftButton = "q" AND "Q"
+            playerRightButton = "d" AND "D"
+            playerBackButton = "s" AND "S"
+            playerShiftButton = ""
+            
+    def move(key) :
+        
+        if key == playerDatas.playerSettings.playerForwardButton :
+            
+            
+            
+        if key == playerDatas.playerSettings.playerLeftButton :
+            
+            
+            
+        if key == playerDatas.playerSettings.playerRightButton :
+            
+            
+            
+        if key == playerDatas.playerSettings.playerBackButton :
+            
+            
+            
+        if key == playerDatas.playerSettings.playerShiftButton :
+            
+            game.animations.shift
+            playerCameraXCord = playerXCord -0.5
+            
+class internet :
+    
+    def internetClient() :
+        
+        
